721b0b4b0aebd7429152a8e93f3625f222f12ab5
Jason Chin <cschin@infoecho.net>
2024-05-03 02:02:22 +0000

    we need to partion the assemble_results.fa to make sure Immunannot finding all genes

---
+++ KIR2/KIR-plot.sh
@@ -0,0 +1,73 @@
+#pgr-pbundle-bed2dist KIR_HPRC.bed KIR_HPRC.
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-100.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC.out -t 20
+
+
+
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-000.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-000.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-012.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-012.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-024.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-024.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-036.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-036.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-048.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-048.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-060.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-060.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-072.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-072.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-084.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-084.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-096.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-096.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-108.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-108.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-120.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-120.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-132.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-132.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-144.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-144.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-156.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-156.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-168.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-168.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-180.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-180.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-192.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-192.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-204.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-204.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-216.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-216.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-228.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-228.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-240.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-240.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-252.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-252.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-264.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-264.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-276.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-276.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-288.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-288.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-300.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-300.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-312.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-312.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-324.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-324.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-336.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-336.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-348.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-348.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-360.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-360.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-372.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-372.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-384.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-384.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-396.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-396.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-408.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-408.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-420.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-420.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-432.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-432.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-444.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-444.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-456.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-456.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-468.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-468.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-480.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-480.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-492.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-492.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-504.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-504.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-516.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-516.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-528.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-528.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-540.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-540.out -t 20
+#bash ../Immuannot/scripts/immuannot.sh -c assemble_results-552.fa -r Data-2023Oct27 -o immuannot_KIR-HPRC-552.out -t 20
+
+cat missing_annotation | awk '{print "grep -A 1 "$0" assemble_results.fa"}'| bash  > contigs_missing_annotation.fa
+bash ../Immuannot/scripts/immuannot.sh -c contigs_missing_annotation.fa -r Data-2023Oct27 -o contigs_missing_annotation.out -t 20
+
+
+
+zcat immuannot_KIR-HPRC-*out.gtf.gz > immuannot_KIR-HPRC.out.gtf
+zcat contigs_missing_annotation.out.gtf.gz >> immuannot_KIR-HPRC.out.gtf
+gzip -f immuannot_KIR-HPRC.out.gtf
+
+python get_gene_seq_from_immuannot.py assemble_results.fa immuannot_KIR-HPRC.out.gtf.gz > KIR-annotation.bed
+
+pgr-pbundle-decomp assemble_results.fa -w 128 -r 12 --min-span 32 --bundle-length-cutoff 120 --min-branch-size 4 -i assemble_results.include assemble_results --min-cov 0 
+
+pgr-pbundle-bed2dist assemble_results.bed assemble_results. -l
+
+pgr-pbundle-bed2svg assemble_results.bed assemble_results. --html --ddg-file assemble_results.ddg --offsets assemble_results.offset --track-panel-width 800 --track-range 350000 --annotation-region-bedfile KIR-annotation.bed --annotation-region-stroke-width 5
+
+#pgr-pbundle-bed2svg assemble_results_filtered.bed assemble_results_f. --html --ddg-file assemble_results.ddg --offsets assemble_results.offset --track-panel-width 800 --track-range 350000 --annotation-region-bedfile KIR-annotation.bed --annotation-region-stroke-width 5
+
+#pgr-alnmap --preset overwrite -w 7 -k 13 -r 1 -m 1   assemble_results.fa kirnmdp.200.fasta prob_aln_test
