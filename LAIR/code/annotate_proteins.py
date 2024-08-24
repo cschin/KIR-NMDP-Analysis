4e170240f93f9d2b16bb9b82cdf16b64cf2e317a
Jason Chin <cschin@infoecho.net>
2024-08-24 19:00:48 +0000

    parsing the exon-intron structure

--- LAIR/code/annotate_proteins.py
+++ LAIR/code/annotate_proteins.py
@@ -1,5 +1,9 @@
 import pgrtk
 import os
+import re
+
+def partition_by_case(s):
+    return re.findall(r'[A-Z]+|[a-z]+', s)
 
 sdb = pgrtk.SeqIndexDB()
 sdb.load_from_fastx("./LAIR_HPRC.fa")
@@ -13,6 +17,8 @@ out_cds_files = {}
 out_cds_files_rank1 = {}
 out_prot_files = {}
 out_prot_files_rank1 = {}
+out_gene_structure_files = {}
+out_gene_structure_files_rank1 = {}
 
 haplotype_to_seq = {}
 for sid in sinfo:
@@ -34,8 +40,14 @@ for key in haplotype_to_seq:
             r = r.strip().split("\t")
             if r[0] == "##ATN":
                 ATN = r[1]
-                seq = ATN.upper()
+                seq = ATN
                 seq = seq.replace("-", "")
+
+                exon_introns = partition_by_case(seq)
+                exons = enumerate([s for s in exon_introns if s[0].isupper()])
+                introns = enumerate([s for s in exon_introns if s[0].islower()])
+
+                seq = ATN.upper()
                 continue
 
             if r[0] == "##PAF":
@@ -63,6 +75,7 @@ for key in haplotype_to_seq:
                         out_seq_files_rank1.setdefault(target, open(f"results/genes/{target}_DNA_rank1.fa", "w"))
                         out_cds_files_rank1.setdefault(target, open(f"results/genes/{target}_CDS_rank1.fa", "w"))
                         out_prot_files_rank1.setdefault(target, open(f"results/genes/{target}_PROT_rank1.fa", "w"))
+                        out_gene_structure_files_rank1.setdefault(target, open(f"results/genes/{target}_Gene_Structure_rank1.txt", "w"))
                     out_seq_file_rank1 = out_seq_files_rank1[target]
                     DNA_seq = seq
                     print(">{}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_seq_file_rank1)
@@ -76,11 +89,21 @@ for key in haplotype_to_seq:
                     print(">{}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_prot_file_rank1)
                     print(STA, file=out_prot_file_rank1)
 
+                    out_gene_structure_file_rank1 = out_gene_structure_files_rank1[target]
+                    print("# {}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_gene_structure_file_rank1)
+                    for rank, s in exons:
+                        print("exon {} {}".format(rank, s), file=out_gene_structure_file_rank1)
+                    for rank, s in introns:
+                        print("introns {} {}".format(rank, s), file=out_gene_structure_file_rank1)
+
+
+
                 print(r[0], r[3], r[4], target, "#000", r[8], file=out_bed_file, sep="\t") 
                 if target not in out_seq_files:
                     out_seq_files.setdefault(target, open(f"results/genes/{target}_DNA.fa", "w"))
                     out_cds_files.setdefault(target, open(f"results/genes/{target}_CDS.fa", "w"))
                     out_prot_files.setdefault(target, open(f"results/genes/{target}_PROT.fa", "w"))
+                    out_gene_structure_files.setdefault(target, open(f"results/genes/{target}_Gene_Structure.txt", "w"))
                 out_seq_file = out_seq_files[target]
                 DNA_seq = seq
                 print(">{}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_seq_file)
@@ -93,6 +116,13 @@ for key in haplotype_to_seq:
                 out_prot_file = out_prot_files[target]
                 print(">{}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_prot_file)
                 print(STA, file=out_prot_file)
+
+                out_gene_structure_file = out_gene_structure_files[target]
+                print("# {}:{}-{} {}".format(ctg, r[3], r[4], r[8]), file=out_gene_structure_file)
+                for rank, s in exons:
+                    print("exon {} {}".format(rank, s), file=out_gene_structure_file)
+                for rank, s in introns:
+                    print("introns {} {}".format(rank, s), file=out_gene_structure_file)
 
 out_bed_file.close()
 out_bed_file_rank1.close()
